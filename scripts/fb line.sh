b21726fd5e697641471c772aed093dd1abd41c2a
zohairshafi <zohairshafi@gmail.com>
2023-01-17 22:53:10 -0500

    Updates

--- scripts/fb line.sh
+++ scripts/fb line.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition=netsi_largemem
 #SBATCH --time=80:00:00
 
-conda activate gpu; /home/shafi.z/.conda/envs/gpu/bin/python /home/shafi.z/ExplainingNodeEmbeddings/scripts/line_experiments.py --graph_path "/home/shafi.z/ExplainingNodeEmbeddings/data/FB15k.pkl" --run_count 10 --hyp_key "hyp_fb15k" --outfile "../results/fb_line.pkl" > ../logs/fb_line.log
+conda activate gpu; /home/shafi.z/.conda/envs/gpu/bin/python /home/shafi.z/ExplainingNodeEmbeddings/scripts/line_experiment.py --graph_path "/home/shafi.z/ExplainingNodeEmbeddings/data/FB15k.pkl" --run_count 5 --hyp_key "hyp_fb15k" --outfile "../results/fb_line.pkl" > ../logs/fb_line.log
 
 
  
--- scripts/pubmed sdne.sh
+++ scripts/pubmed sdne.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition=netsi_largemem
 #SBATCH --time=80:00:00
 
-conda activate gpu; /home/shafi.z/.conda/envs/gpu/bin/python /home/shafi.z/ExplainingNodeEmbeddings/scripts/sdne_experiments.py --graph_path "/home/shafi.z/ExplainingNodeEmbeddings/data/pubmed.pkl" --run_count 10 --hyp_key "hyp_pubmed" --outfile "../results/pubmed_sdne.pkl" > ../logs/pubmed_sdne.log
+conda activate gpu; /home/shafi.z/.conda/envs/gpu/bin/python /home/shafi.z/ExplainingNodeEmbeddings/scripts/sdne_experiment.py --graph_path "/home/shafi.z/ExplainingNodeEmbeddings/data/pubmed.pkl" --run_count 5 --hyp_key "hyp_pubmed" --outfile "../results/pubmed_sdne.pkl" > ../logs/pubmed_sdne.log
 
 
  
