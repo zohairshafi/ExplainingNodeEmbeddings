7051ef2d4fb3af2ebd74f0c87291724e4f953371
zohairshafi <zohairshafi@gmail.com>
2023-01-19 21:57:49 -0500

    Updates

--- scripts/pubmed gmi.sh
+++ scripts/pubmed gmi.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition=netsi_largemem
 #SBATCH --time=80:00:00
 
-conda activate gpu; /home/shafi.z/.conda/envs/gpu/bin/python /home/shafi.z/ExplainingNodeEmbeddings/scripts/gmi_experiments.py --graph_path "/home/shafi.z/ExplainingNodeEmbeddings/data/pubmed.pkl" --run_count 5 --hyp_key "hyp_pubmed" --use_id 'False' --outfile "../results/pubmed_gmi.pkl" > ../logs/pubmed_gmi.log
+conda activate gpu; /home/shafi.z/.conda/envs/gpu/bin/python /home/shafi.z/ExplainingNodeEmbeddings/scripts/gmi_experiments.py --graph_path "/home/shafi.z/ExplainingNodeEmbeddings/data/pubmed.pkl" --run_count 5 --hyp_key "hyp_pubmed" --use_id 'False' --update_outfile "False" --outfile "../results/pubmed_gmi.pkl" > ../logs/pubmed_gmi.log
 
 
  
