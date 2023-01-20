b6b6dd13d3d39355a5b3a53867961f18c75b65fa
zohairshafi <zohairshafi@gmail.com>
2023-01-19 22:15:07 -0500

    Updates

--- scripts/citeseer gmi.sh
+++ scripts/citeseer gmi.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=36
 #SBATCH --job-name=citeseer_gmi
 #SBATCH --mem=32G
-#SBATCH --partition=netsi_largemem
+#SBATCH --partition=netsi_standard
 #SBATCH --time=80:00:00
 
 conda activate gpu; /home/shafi.z/.conda/envs/gpu/bin/python /home/shafi.z/ExplainingNodeEmbeddings/scripts/gmi_experiments.py --graph_path "/home/shafi.z/ExplainingNodeEmbeddings/data/citeseer.pkl" --run_count 5 --hyp_key "hyp_citeseer" --use_id 'False' --update_outfile "False" --outfile "../results/citeseer_gmi.pkl" > ../logs/citeseer_gmi.log
--- scripts/pubmed gmi.sh
+++ scripts/pubmed gmi.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=48
 #SBATCH --job-name=pubmed_gmi
-#SBATCH --mem=128G
-#SBATCH --partition=netsi_largemem
+#SBATCH --mem=64G
+#SBATCH --partition=netsi_standard
 #SBATCH --time=80:00:00
 
 conda activate gpu; /home/shafi.z/.conda/envs/gpu/bin/python /home/shafi.z/ExplainingNodeEmbeddings/scripts/gmi_experiments.py --graph_path "/home/shafi.z/ExplainingNodeEmbeddings/data/pubmed.pkl" --run_count 5 --hyp_key "hyp_pubmed" --use_id 'False' --update_outfile "False" --outfile "../results/pubmed_gmi.pkl" > ../logs/pubmed_gmi.log
